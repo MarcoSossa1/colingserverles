1cae43285b3e3b677e594341c5088456cb890ada
MarcoSossa1 <tj.marco.sossa.z@upds.net.bo>
2024-03-13 10:32:47 -0400

    ApiAfiliados con todos sus metodos

---
+++ Coling/Coling.API/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coling.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Identity.Client;
+namespace Coling.API.Afiliados
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base(options) 
+        {
+            
+        }
+        public virtual DbSet<Persona> Personas { get; set; }
+    }
+}
