ccf78208d4a2a8089e708faf29c1520b5d9eb8df
MarcoSossa1 <tj.marco.sossa.z@upds.net.bo>
2024-03-13 17:23:21 -0400

    Api de Afiliados Clase Afiliado con todos sus metodos

---
+++ Coling/Coling.API/Contratos/IAfiliadoLogic.cs
@@ -0,0 +1,15 @@
+﻿using Coling.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Coling.API.Afiliados.Contratos
+{
+    public interface IAfiliadoLogic
+    {
+        Task<bool> InsertarAfiliado(Afiliado afiliado);
+        Task<bool> ModificarAfiliado(Afiliado afiliado, int id);
+        Task<bool> EliminarAfiliado(int id);
+        Task<List<Afiliado>> ListarAfiliados();
+        Task<Afiliado> ObtenerAfiliadoById(int id);
+    }
+}
