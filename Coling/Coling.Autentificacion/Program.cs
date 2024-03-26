71ff9964ca9a2d1abf47359c18062165070bd307
MarcoSossa1 <tj.marco.sossa.z@upds.net.bo>
2024-03-25 21:14:14 -0400

    JWT Agregados a los microservicios

--- Coling/Coling.Autentificacion/Program.cs
+++ Coling/Coling.Autentificacion/Program.cs
@@ -11,6 +11,7 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
     })
     .Build();
 
